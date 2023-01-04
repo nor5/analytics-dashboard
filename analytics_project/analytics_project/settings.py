28d6761422c03602494e18903d77d145714aab19
nor5 <noorbelkacem5@gmail.com>
2023-01-04 12:58:18 +0100

    fixing warning

--- analytics_project/analytics_project/settings.py
+++ analytics_project/analytics_project/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n6e7kkvo0ea$1bxdvfi)y2k#5%ksoz^9)!od440f0x4c4$$c@e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
