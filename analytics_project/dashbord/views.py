47fc2497954f577e2f569d42261511cbd6d78f1e
nor5 <noorbelkacem5@gmail.com>
2023-01-04 10:27:47 +0100

    pivot_data

--- analytics_project/dashbord/views.py
+++ analytics_project/dashbord/views.py
@@ -5,3 +5,8 @@ from django.core import serializers
 
 def dashboard_with_pivot(request):
     return render(request, 'dashboard_with_pivot.html', {})
+
+def pivot_data(request):
+    dataset = Order.objects.all()
+    data = serializers.serialize('json', dataset)
+    return JsonResponse(data, safe=False)
