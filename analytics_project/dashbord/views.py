b4b6409aaeb1a170f1c7e8f140eadff6257fe6ea
nor5 <noorbelkacem5@gmail.com>
2023-01-04 12:51:52 +0100

    dash views

--- analytics_project/dashbord/views.py
+++ analytics_project/dashbord/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from dashboard.models import Order
+from dashbord.models import Order
 from django.core import serializers
 
 def dashboard_with_pivot(request):
